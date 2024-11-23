88e2356d2f567034fcbc5d04f266fc86d4b53597
marlon-erler <viridian035@gmail.com>
2024-11-23 13:37:52 +0000

    add modules and structure

---
+++ index.ts
@@ -0,0 +1,16 @@
+import Fs from "fs/promises";
+import Path from "path";
+
+export class Database {
+    basePath: string;
+
+    constructor() {
+    }
+
+    createTable() {}
+    deleteTable() {}
+
+    addEntry() {}
+    changeEntry() {}
+    deleteEntry() {}
+}
