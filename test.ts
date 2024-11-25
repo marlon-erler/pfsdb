b408a34ad6f2d3287d6cb1e95e1ebd48ac0e341e
marlon-erler <viridian035@gmail.com>
2024-11-25 14:31:33 +0000

    add assertArrays function, implement test for getDirectoryPath method

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-console.log("starting test...");
+import { Util, Database, Table, Entry } from "./index";
 
 function logStep(stepName: string) {
     console.log(`
@@ -8,4 +8,30 @@ function logStep(stepName: string) {
     )
 }
 
-logStep("generate directory path");
+function assertArrays(a: any[], b: any[]) {
+    console.log("comparing arrays", a, b);
+
+    if (a.length != b.length) 
+	throw "array sizes do not mach"
+
+    for (let i = 0; i < a.length; i++) {
+	const aItem = a[i];
+	const bItem = b[i];
+
+	if (aItem == bItem) 
+	    continue;
+
+	throw `${aItem} does not match ${bItem} (index ${i}`;
+    }
+}
+
+console.log("starting test...");
+
+{
+    logStep("generate directory path");
+
+    const filePath = ["a", "b", "c"];
+    const control = ["a", "b"];
+    const result = Util.getDirectoryPath(filePath);
+    assertArrays(control, result);
+}
